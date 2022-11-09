42bec188ea725b3159179c059cdc8377604287c0
Mahbuba-Akhter-Ema <mahbubaakhter22@gmail.com>
2022-11-10 04:20:52 +0600

    myReview added

--- index.js
+++ index.js
@@ -12,51 +12,68 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.trknobu.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run (){
-    try{
+async function run() {
+    try {
         const serviceCollection = client.db('muslimah').collection('services');
-        // const reviewCollection = client.db('muslimah').collection('review')
-        app.get('/services', async(req, res)=>{
-            const query={};
-            const cursor=await serviceCollection.find(query);
-            const service= await cursor.limit(3).toArray();
+        const reviewCollection = client.db('muslimah').collection('review')
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = await serviceCollection.find(query);
+            const service = await cursor.limit(3).toArray();
             res.send(service);
-        });         
-        app.get('/allService', async(req, res)=>{
-            const query={};
-            const cursor=await serviceCollection.find(query);
-            const service= await cursor.toArray();
+        });
+        app.get('/allService', async (req, res) => {
+            const query = {};
+            const cursor = await serviceCollection.find(query);
+            const service = await cursor.toArray();
             res.send(service);
-        }); 
-        app.get('/allService/:id', async(req, res)=>{
+        });
+        app.get('/allService/:id', async (req, res) => {
             const id = req.params.id;
-            const query={_id:ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const service = await serviceCollection.findOne(query);
             res.send(service);
         });
-        // app.get("/review", async (req, res) => {
-        //     let query = {};
-            
-        //     if (req.query.serviceID) {
-        //       query = { serviceID: req.query.serviceID };
-        //     }
-      
-        //     const cursor = await reviewCollection.find(query);
-        //     const result = await cursor.toArray();
-        //     res.send(result);
-        //   });       
+
+        app.get("/review", async (req, res) => {
+            let query = {};
+
+            if (req.query.serviceID) {
+                query = { serviceID: req.query.serviceID };
+            }
+
+            const cursor = await reviewCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        // post review......
+        app.post("/review", async (req, res) => {
+            const review = req.body;
+            const cursor = await reviewCollection.insertOne(review);
+            res.send(cursor);
+        });
+        app.get("/myReview", async (req, res) => {
+            let query = {};
+            if (req.query.email) {
+              query = { email: req.query.email};
+            }
+            const cursor = reviewCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+          });
     }
-    finally{
-       
+    finally {
+
     }
 }
 run().catch(err => console.log(err));
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Muslimah Server Running!');
 });
 
 app.listen(port, () => {
-        console.log(`Muslimah Server listening on ${port}`);
+    console.log(`Muslimah Server listening on ${port}`);
 })
